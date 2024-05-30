betakurnia/Git-challenge-v2

feat: add base64 endpoint

server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
 
   res.end()
 })
@@ -16,6 +17,10 @@ function respondHello (req, res) {
 function respondUserAgent (req, res) {
   const ua = req.headers['user-agent']
   res.end(JSON.stringify({ ua }))
+}
+function respondBase64 (req, res) {
+  const phrase = req.url.replace(/^\/b64\//, '')
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
 }
 
 server.listen(PORT)

jorgeluis1985/git-challenge

server.js
@@ -5,17 +5,22 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url.match(/^\/b64\//)) return respondBase64(req, res)
 
   res.end()
 })
 
-function respondHello (req, res) {
+function respondHello(req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
 }
 
-function respondUserAgent (req, res) {
+function respondUserAgent(req, res) {
   const ua = req.headers['user-agent']
   res.end(JSON.stringify({ ua }))
+}
+function respondBase64(req, res) {
+  const phrase = req.url.replace(/^\/b64\//, '')
+  res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
 }
 
 server.listen(PORT)